96beb22b7f364183c1f94f0650a970c1900264bc
Jake Patock <jake.r.patock@gmail.com>
2023-12-07 14:35:37 -0600

    update

--- src/model_train_test.py
+++ src/model_train_test.py
@@ -8,35 +8,11 @@ import sklearn.metrics as skm
 import sklearn.model_selection as skms
 import torch.utils.data as tud
 
-def standardize (X):
-    """
-    X is a 2D Numpy array that each row is a feature vector of a data point (columns are features)
-    Returns a standardized 2D Numpy array 
-    """
-    #cacluting mean along the first axis 
-    feature_mean = X.mean(0)
-    #calculating the mean along the first axis 
-    feature_std = X.std(0)
-    #standardize the features
-    return (X - feature_mean) / feature_std
-    
-#set up the data 
-data = pd.read_csv('C:\Visual Studio Coding\pytorch\linear regression\Charges_Prediction\data\insurance.csv')
-#getting categoires to convert to numeric values 
-categories = ['sex', 'smoker', 'region']
-#converting these pd collumns to numeric
-data = pd.get_dummies(data, columns=categories)
-
-#getting training data and labels .values returns a numpy array, using astype(float) converts all true to 1 and all false to 0 
-features = standardize(data.drop('charges', axis=1).values.astype(float))
-labels = standardize(data['charges'].values.astype(float))
-
-#features 
-features_tensor = torch.tensor(features, dtype=torch.float32)
-labels_tensor = torch.tensor(labels, dtype=torch.float32)
-
-#inputs two features tensor and labels tensor, test_size determines the percentage of the data to use for test, random_state is the seed of the random spliting 
-train_features, test_features, train_labels, test_labels = skms.train_test_split(features_tensor, labels_tensor, test_size=0.2, random_state=42)
+#loading saved tensor from data 
+train_features = torch.load(r'C:\Visual Studio Coding\pytorch\linear regression\Charges_Prediction\data\train_features.pt')
+train_labels = torch.load(r'C:\Visual Studio Coding\pytorch\linear regression\Charges_Prediction\data\train_labels.pt')
+test_features = torch.load(r'C:\Visual Studio Coding\pytorch\linear regression\Charges_Prediction\data\test_features.pt')
+test_labels = torch.load(r'C:\Visual Studio Coding\pytorch\linear regression\Charges_Prediction\data\test_labels.pt')
 
 #create torch dataset, takes two torch tensors, the features tensor and the labels tensor 
 train_data = tud.TensorDataset(train_features, train_labels)
@@ -44,12 +20,12 @@ test_data = tud.TensorDataset(test_features, test_labels)
 
 
 #taking tensor datasets and feeding them to data loader, this returns a iterable of batches of data
-batch_size = 64
+batch_size = 128
 train_loader = tud.DataLoader(train_data, batch_size=batch_size, shuffle=True)
 test_loader = tud.DataLoader(test_data, batch_size=batch_size)
 
 #setting up the model 
-n_samples, n_features = features.shape
+n_samples, n_features = train_features.shape
 x_para = n_features
 y_para = 1 
 model = nn.Linear(x_para, y_para)
@@ -58,10 +34,10 @@ model = nn.Linear(x_para, y_para)
 loss_func = nn.MSELoss()
 
 #learning rate not needed for Adam, RMSprop
-optimizer = torch.optim.RMSprop(model.parameters())
+optimizer = torch.optim.Adam(model.parameters())
 
 #training loop
-epochs = 10000
+epochs = 1001
 for epoch in range(epochs):
     #set model into training mode 
     model.train()
@@ -85,7 +61,9 @@ for epoch in range(epochs):
 
     if epoch % 1000 == 0:
         print(f"Training Loss: {loss}")
-    
+
+torch.save(model, 'data/model.pth')
+
 model.eval()
 test_loss_cul = 0
 r_squared_total = 0 
@@ -106,11 +84,6 @@ with torch.no_grad():
 
     test_loss = test_loss_cul / len(test_loader.dataset)
     average_r_squared = r_squared_total / len(test_loader)
-    
+        
     print(f"Test Loss: {test_loss}")
     print(f"R2 Value: {average_r_squared}")
-
-
-    # # #casting the list to a float tensor to use 
-    # # test = torch.FloatTensor([23, 0, 22.8, 0, 0, 0])
-    # # print(f"The test case of 18 = {model(test) * 1000}")
