09c579db577254acc3f95327b04065336748e17d
Jake Patock <jake.r.patock@gmail.com>
2023-12-07 13:42:37 -0600

    update

--- src/lr_model_health_insurance.py
+++ src/lr_model_health_insurance.py
@@ -4,9 +4,9 @@ import torch.nn as nn
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
-import torchmetrics.functional as tm
-
-
+import sklearn.metrics as skm
+import sklearn.model_selection as skms
+import torch.utils.data as tud
 
 def standardize (X):
     """
@@ -18,83 +18,100 @@ def standardize (X):
     #calculating the mean along the first axis 
     feature_std = X.std(0)
     #standardize the features
-    standard_X = (X - feature_mean) / feature_std
-    return standard_X
+    return (X - feature_mean) / feature_std
+    
 
 #set up the data 
-data = pd.read_csv('C:\Visual Studio Coding\pytorch\linear regression\data\insurance.csv')
+data = pd.read_csv('C:\Visual Studio Coding\pytorch\linear regression\Charges_Prediction\data\insurance.csv')
+#getting categoires to convert to numeric values 
 categories = ['sex', 'smoker', 'region']
+#converting these pd collumns to numeric
 data = pd.get_dummies(data, columns=categories)
 
-training_data = data.drop('charges', axis=1).values
-labels = data['charges'].values
+#getting training data and labels .values returns a numpy array, using astype(float) converts all true to 1 and all false to 0 
+features = standardize(data.drop('charges', axis=1).values.astype(float))
+labels = standardize(data['charges'].values.astype(float))
+
+#features 
+features_tensor = torch.tensor(features, dtype=torch.float32)
+labels_tensor = torch.tensor(labels, dtype=torch.float32)
+
+#inputs two features tensor and labels tensor, test_size determines the percentage of the data to use for test, random_state is the seed of the random spliting 
+train_features, test_features, train_labels, test_labels = skms.train_test_split(features_tensor, labels_tensor, test_size=0.2, random_state=42)
+
+#create torch dataset, takes two torch tensors, the features tensor and the labels tensor 
+train_data = tud.TensorDataset(train_features, train_labels)
+test_data = tud.TensorDataset(test_features, test_labels)
+
+#taking tensor datasets and feeding them to data loader, this returns a iterable of batches of data
+batch_size = 64
+train_loader = tud.DataLoader(train_data, batch_size=batch_size, shuffle=True)
+test_loader = tud.DataLoader(test_data, batch_size=batch_size)
 
 
+#setting up the model 
+n_samples, n_features = features.shape
+x_para = n_features
+y_para = 1 
+model = nn.Linear(x_para, y_para)
+
+#setting up the loss and optimizer 
+loss_func = nn.MSELoss()
+
+#learning rate not needed for Adam, RMSprop
+optimizer = torch.optim.RMSprop(model.parameters())
+
+#training loop
+epochs = 10000
+for epoch in range(epochs):
+    #set model into training mode 
+    model.train()
+    #setting running loss
+    running_loss = 0 
+    for features, labels in train_loader:
+        #clear optimizer
+        optimizer.zero_grad()
+        #get y predicted
+        predicted_labels = model(features)
+        #get loss 
+        loss = loss_func(predicted_labels, labels.view(-1, 1))
+        #calculated direction of gradient
+        loss.backward()
+        #get the new theta
+        optimizer.step()
+        #multipe the loss by the size of the batch to scale it to make the loss 
+        #representative of the loss across the entire batch (accounts for batch of different sizes)
+        running_loss += loss.item() * features.size(0)
+    loss = running_loss / len(train_loader.dataset)
+
+    if epoch % 1000 == 0:
+        print(f"Training Loss: {loss}")
+    
+model.eval()
+test_loss_cul = 0
+r_squared_total = 0 
+with torch.no_grad():
+    for features, labels in test_loader:
+        #calculating predicted 
+        predicted_labels = model(features)
+
+        labels = labels.view(-1)
+        predicted_labels = predicted_labels.view(-1)
+
+        loss = loss_func(predicted_labels, labels)
+        test_loss_cul += loss.item() * features.size(0) 
+
+        #calculating the r squared score 
+        r_squared_score = skm.r2_score(labels, predicted_labels)
+        r_squared_total += r_squared_score
+
+    test_loss = test_loss_cul / len(test_loader.dataset)
+    average_r_squared = r_squared_total / len(test_loader)
+    
+    print(f"Test Loss: {test_loss}")
+    print(f"R2 Value: {average_r_squared}")
 
 
-#converting the data to numeric 
-#male = 0, female = 1
-x_data[x_data == 'male'] = 0
-x_data[x_data == 'female'] = 1
-
-#non-smoker = 0, smoker = 1, 
-x_data[x_data == 'no'] = 0
-x_data[x_data == 'yes'] = 1
-
-# #northeast = 0, southeast = 1, northwest = 2, southwest = 3
-# x_data[x_data == 'northeast'] = 0
-# x_data[x_data == 'southeast'] = 1
-# x_data[x_data == 'northwest'] = 2
-# x_data[x_data == 'southwest'] = 3
-
-
-# #standardizing the data 
-# x_numpy = standardize(x_data.astype(np.float32))
-# y_numpy = standardize(y_data.astype(np.float32))
-
-
-# #converting the data to torch tensor 
-# x = torch.from_numpy(x_numpy.astype(np.float32)) 
-# y = torch.from_numpy(y_numpy.astype(np.float32)) 
-
-# #setting up the model 
-# n_samples, n_features = x.shape
-# x_para = n_features
-# y_para = 1 
-# lr_model = nn.Linear(x_para, y_para)
-
-# #setting up the loss and optimizer 
-# loss_func = nn.MSELoss()
-
-# #learning rate not needed for Adam, RMSprop
-# optimizer = torch.optim.Adam(lr_model.parameters())
-
-# #training loop
-# epochs = 10000
-# for epoch in range(epochs):
-#     #get y predicted
-#     y_predicted = lr_model(x)
-#     #get loss 
-#     loss = loss_func(y_predicted, y)
-#     #calculated direction of gradient
-#     loss.backward()
-#     #get the new theta
-#     optimizer.step()
-#     #clear optimizer
-#     optimizer.zero_grad()
-
-#     if epoch % 100 == 0:
-#         print(loss.item())
-
-# print('here')
-
-# #getting regression line 
-# theta = lr_model(x).detach().numpy()
-
-# #calculating the r squared score 
-# r_squared_score = tm.r2_score(theta, y)
-# print(f"The R-squared score is {r_squared_score}")
-
-# #casting the list to a float tensor to use 
-# test = torch.FloatTensor([23, 0, 22.8, 0, 0, 0])
-# print(f"The test case of 18 = {lr_model(test) * 1000}")
+    # # #casting the list to a float tensor to use 
+    # # test = torch.FloatTensor([23, 0, 22.8, 0, 0, 0])
+    # # print(f"The test case of 18 = {model(test) * 1000}")
